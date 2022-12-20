efce4aa19dda32aaea74d992543ca618c5d3950d
Zengo Iori <69503288+shyu-sui@users.noreply.github.com>
2022-12-20 11:02:47 +0900

    first

---
+++ website/button.js
@@ -0,0 +1,21 @@
+function btnClick(){
+  SerchPlace1 = document.getElementById("test");
+    if(test.style.display == "block"){
+      // noneで非表示
+      test.style.display = "none";
+    }else{
+      // blockで表示
+      test.style.display = "block";
+  }
+}
+
+function btnClick(){
+  SerchPlace2 = document.getElementById("test");
+    if(test.style.display == "block"){
+      // noneで非表示
+      test.style.display = "none";
+    }else{
+      // blockで表示
+      test.style.display = "block";
+  }
+}
